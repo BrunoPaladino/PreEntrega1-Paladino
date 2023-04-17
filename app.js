14ab607a742eefe75efdf5696e6805140e35d375
Bruno Paladino <brunos_paladino@hotmail.com>
2023-04-17 00:35:34 -0300

    Arreglo resultado

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ let i;
 alert("Bienvenido al centro Pokemon, nos encargamos del cuidado de sus pokemons.")
 ingresoSalud(salud, tratamiento, cantidad, i);
 
-function ingresoSalud(salud) {
-    salud = prompt("Ingrese los puntos de salud (PS) de su pokemon (valor entre 0 y 100):");
+function ingresoSalud(salud, tratamiento) {
+    salud = Number(prompt("Ingrese los puntos de salud (PS) de su pokemon (valor entre 0 y 100):"));
 
     while (salud < 0 || salud > 100 || salud == " ") {
-        salud = prompt("Ingrese un valor valido, entre 0 y 100, para los PS:");
+        salud = Number(prompt("Ingrese un valor valido, entre 0 y 100, para los PS:"));
     }
     if (salud == 100) {
         console.log("Su pokemon esta en perfecto estado, no requiere de nuestros servicios.");
         pokemonSano();
     } else {
-        ingresoTratamiento(tratamiento);
+        ingresoTratamiento(salud, tratamiento);
     }
 }
 
@@ -24,7 +24,7 @@ function pokemonSano() {
     console.log("Los puntos de salud actuales de su pokemon son: 100");
 }
 
-function ingresoTratamiento(tratamiento) {
+function ingresoTratamiento(salud, tratamiento) {
     tratamiento = prompt("Seleccione que tratamiento darle a su pokemon:\n 1- Pocion (restaura 20 PS);\n 2- Superpocion (restaura 60 PS);\n 3- Sanacion completa;");
     while (tratamiento != 1 && tratamiento != 2 && tratamiento != 3) {
         tratamiento = prompt("Seleccione una opcion valida:\n 1- Pocion (restaura 20 PS);\n 2- Superpocion (restaura 60 PS);\n 3- Sanacion completa;\n");
@@ -59,18 +59,18 @@ function resultado(salud, cantidad, tratamiento) {
     if ((tratamiento == 1) && (salud >= 100)) {
         console.log(`Felicitaciones, el tratamiento ha finalizado. Se aplicaron: ${cantidad} pocion/es a su pokemon`);
         console.log(`Los puntos de salud actuales de su pokemon son: 100`);
-        }   else if ((tratamiento == 1) && (salud < 100)) {
+        } else if ((tratamiento == 1) && (salud < 100)) {
             console.log(`Felicitaciones, el tratamiento ha finalizado. Se aplicaron: ${cantidad} pocion/es a su pokemon.`)
             console.log(`Los puntos de salud actuales de su pokemon son: ${salud}`);
     }
 
-/*     if (tratamiento == 2 && salud >= 100) {
+    if ((tratamiento == 2) && (salud >= 100)) {
         console.log(`Felicitaciones, el tratamiento ha finalizado. Se aplicaron: ${cantidad} superpocion/es a su pokemon.`)
         console.log("Los puntos de salud actuales de su pokemon son: 100");
-        }  else if (tratamiento == 2 && salud < 100) {
+        } else if ((tratamiento == 2) && (salud < 100)) {
             console.log(`Felicitaciones, el tratamiento ha finalizado. Se aplicaron: ${cantidad} superpocion/es a su pokemon.`)
             console.log(`Los puntos de salud actuales de su pokemon son: ${salud}`);
-    } */
+    }
 
     if (tratamiento == 3) {
         console.log("Felicitaciones, el tratamiento ha finalizado.")
